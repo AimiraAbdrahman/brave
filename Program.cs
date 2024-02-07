d21d050a2163ab25fbf2e8c13c71573703bcfdc0
Aimira <aimira.abdrahman@gmail.com>
2024-02-07 21:39:53 +0500

    added first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трехзначное число
+// и удаляет вторую цифру этого числа.
+// Примеры 
+// а = 256 => 26
+// a = 891 => 81
+
+int number = 453;
+int firstDigit = number / 100; // 453 / 100 = 4
+int thirdDigit = number % 10; // 453 / 10 = 3
+
+int result = firstDigit * 10 + thirdDigit; // 4*10 + 3 = 43
+Console.WriteLine($"В числе {number} => {result}");
